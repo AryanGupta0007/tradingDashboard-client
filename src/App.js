318e6740218e29253c3931595ec1fd0d48729b07
Aryan <aryangupta07075@gmail.com>
2025-02-28 16:42:52 +0530

    starting with websocket

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import io from 'socket.io-client'
+import {useEffect} from 'react'
 import './App.css';
 import './utils.css'
 import './sections.css'
@@ -9,8 +11,13 @@ import {Footer} from './sections/Footer.js'
 import {TableState} from './contexts/TableContext.js'
 import {MarketWatchState} from './contexts/MarketWatchContext.js'
 import {FormState} from './contexts/FormContext.js'
-
+const socket = io('http://localhost:5001/', {transports: ['websocket']})
 function App() {
+    useEffect(() => {
+        socket.on('connect', () => {
+            console.log('connected to server')
+        })
+    })
     return (
         <TableState>
             <MarketWatchState>
